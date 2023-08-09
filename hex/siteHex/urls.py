ce9ba139ea726c6ed61bdacd151ce7c376af3bad
Igor Nascimento <trabalho.computador.sjn@gmail.com>
2023-08-09 19:54:45 -0300

    entrega total do site funcionando e testado

--- hex/siteHex/urls.py
+++ hex/siteHex/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('about/',views.about, name="about"),
+    path('product',views.product, name='produto'),
+    path('products',views.products, name='produtos'),
+    path('contato/',views.contact, name='contato'),
 ]
--- hex/siteHex/views.py
+++ hex/siteHex/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def product(request):
+    return render(request, 'single-product.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def contact(request):
+    return render(request, 'contact.html')
